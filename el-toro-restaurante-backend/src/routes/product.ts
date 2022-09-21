ef90c30112e5a8490d278f92e8c62cde59cc20ca
Elian Gonzalez <eliancarlogm@gmail.com>
2022-09-20 21:57:39 -0400

    push changes

--- el-toro-restaurante-backend/src/routes/product.ts
+++ el-toro-restaurante-backend/src/routes/product.ts
@@ -1,6 +1,11 @@
 const router = require("express").Router();
-
-const { createProduct, getAllProducts } = require("../controller/products");
+const {
+   createProduct,
+   getAllProducts,
+   getProductById,
+   editProductById,
+   deleteProduct,
+} = require("../controller/products");
 
 const multer = require("multer");
 const path = require("path");
@@ -8,7 +13,7 @@ const shortid = require("shortid");
 
 const storage = multer.diskStorage({
    destination: function (req: any, file: any, cb: any) {
-      cb(null, path.join(path.dirname(__dirname), "../uploads"));
+      cb(null, path.join(path.dirname(__dirname), "./uploads"));
    },
    filename: function (req: any, file: any, cb: any) {
       cb(null, `${shortid.generate()}-${file.originalname}`);
@@ -16,13 +21,20 @@ const storage = multer.diskStorage({
 });
 //console.log(path.dirname(__dirname));
 const upload = multer({ storage });
-console.log(path.dirname(__dirname));
+//console.log(path.dirname(__dirname));
 
 router.get("/admin/products", getAllProducts);
+router.get("/admin/product/:id", getProductById);
 router.post(
    "/admin/product/create",
    upload.array("productPicture", 7),
    createProduct
 );
+router.put(
+   "/admin/product/edit/:id",
+   upload.array("productPicture", 7),
+   editProductById
+);
+router.delete("/admin/product/:id", deleteProduct);
 
 export default router;
